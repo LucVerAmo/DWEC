7a413df3199eb9ab5b3e9d44b0c96102fb019280
Lucas Vera <lucveramo@alu.edu.gva.es>
2023-11-21 19:57:33 +0100

    Never gonna let you down

---
+++ Cliente/Examen/examen.js
@@ -0,0 +1,14 @@
+let ordenadores = new Array;
+
+if(isNaN(Date.parse(fecha)) == true){
+
+}
+if (intFecha == undefined) {
+    let fecha1 = new Date;
+    this.fecha = Date.parse(fecha1);
+}
+else {
+    this.fecha = Date.parse(intFecha);
+}
+
+function ultimoejercicio()
