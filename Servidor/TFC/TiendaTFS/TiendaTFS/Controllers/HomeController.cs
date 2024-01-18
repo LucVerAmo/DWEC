4153afaf451baaeb33adb330dbe3f8df8c31beea
Lucas Vera <lucveramo@alu.edu.gva.es>
2024-01-18 18:32:41 +0100

    Never gonna give you up

--- Servidor/TFC/TiendaTFS/TiendaTFS/Controllers/HomeController.cs
+++ Servidor/TFC/TiendaTFS/TiendaTFS/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TiendaTFS.Data;
 using TiendaTFS.Models;
 
 namespace TiendaTFS.Controllers
@@ -7,14 +8,27 @@ namespace TiendaTFS.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MvcTiendaTFSContexto _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MvcTiendaTFSContexto context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            // Busca el empleado correspondiente al usuario actual. Si existe, activa la
+            // vista (View) y en caso contrario, se redirige para crear el empleado.
+            string? emailUsuario = User.Identity.Name;
+            Cliente? cliente = _context.Clientes.Where(e => e.Email == emailUsuario)
+            .FirstOrDefault();
+            if (User.Identity.IsAuthenticated &&
+            User.IsInRole("Usuario") &&
+            cliente == null)
+            {
+                return RedirectToAction("Create", "MisDatos");
+            }
             return View();
         }
 
---
+++ Servidor/TFC/TiendaTFS/TiendaTFS/Controllers/MisDatosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using TiendaTFS.Data;
+using TiendaTFS.Models;
+
+namespace TiendaTFS.Controllers
+{
+    [Authorize(Roles = "Usuario")]
+    public class MisDatosController : Controller
+    {
+        private readonly MvcTiendaTFSContexto _context;
+        public MisDatosController(MvcTiendaTFSContexto context)
+        {
+            _context = context;
+        }
+        // GET: MisDatos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST: MisDatos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>
+       Create([Bind("Id,Nombre,Email,Telefono,Poblacion")] Cliente cliente)
+        {
+            // Asignar el Email del usuario actual
+            cliente.Email = User.Identity.Name;
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(cliente);
+        }
+    }
+}
