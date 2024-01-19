7664b3c0f2000e2992d9fd5517528076a22192b0
Lucas Vera <lucveramo@alu.edu.gva.es>
2024-01-19 16:51:21 +0100

    Poco a poco

--- Servidor/TFC/TiendaTFS/TiendaTFS/Controllers/MisDatosController.cs
+++ Servidor/TFC/TiendaTFS/TiendaTFS/Controllers/MisDatosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
 using TiendaTFS.Data;
 using TiendaTFS.Models;
@@ -34,6 +35,57 @@ namespace TiendaTFS.Controllers
                 return RedirectToAction("Index", "Home");
             }
             return View(cliente);
+        }
+        // GET: MisDatos/Edit
+        public async Task<IActionResult> Edit()
+        {
+            // Se seleccionan los datos del empleado correspondiente al usuario actual
+            string? emailUsuario = User.Identity.Name;
+            Cliente? cliente = await _context.Clientes
+            .Where(e => e.Email == emailUsuario)
+            .FirstOrDefaultAsync();
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Edit(int id,
+            [Bind("Id, Nombre, Email, Telefono, Poblacion")] Cliente cliente)
+        {
+            if (id != cliente.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(cliente);
+        }
+        private bool ClienteExists(int id)
+        {
+            return (_context.Clientes?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
---
+++ Servidor/TFC/TiendaTFS/TiendaTFS/Controllers/MisPedidosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using TiendaTFS.Data;
+using TiendaTFS.Models;
+
+namespace TiendaTFS.Controllers
+{
+    public class MisPedidosController : Controller
+    {
+        [Authorize(Roles = "Usuario")]
+        public async Task<IActionResult> Index()
+        {
+            // Se selecciona el empleado correspondiente al usuario actual
+            var emailUsuario = User.Identity.Name;
+            var cliente = await _context.Cliente.Where(e => e.Email == emailUsuario)
+            .FirstOrDefaultAsync();
+            if (cliente == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            // Se seleccionan los avisos del Empleado correspondiente al usuario actual
+            var misPedidos = _context.Pedido
+            .Where(a => a.ClienteId == cliente.Id)
+            .OrderByDescending(a => a.Fecha)
+            .Include(a => a.Cliente);
+            return View(await misPedidos.ToListAsync());
+        }
+
+    }
+}
