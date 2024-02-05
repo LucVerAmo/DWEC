352a61f7c4c2cc215ceca67707b1b731ff628229
Lucas Vera <lucveramo@alu.edu.gva.es>
2024-02-05 16:47:39 +0100

    Espero que funcione

---
+++ Servidor/TFC/TiendaTFS/TiendaTFS/Models/Carrito.cs
@@ -0,0 +1,9 @@
+﻿namespace TiendaTFS.Models
+{
+    public class Carrito
+    {
+        public int Id { get; set; }
+        public Producto Producto { get; set; }
+        public Pedido Pedido { get; set; }
+    }
+}
